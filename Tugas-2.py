c5a0a45dd2eee91f54fb63b916120e7e5d04662c
indiamr <110322470+indiamr@users.noreply.github.com>
2022-08-13 16:06:08 +0700

    Add files via upload

---
+++ Tugas-2.py
@@ -0,0 +1,35 @@
+name = [ ' ' ]
+notlp= [ ' ' ]
+
+#Contact list code
+def contactlist( ) :
+	print("Contact List : \n")
+	for n in range (len(name)):
+		print ("Name  : " + name[n])
+		print ("Phone No :  " + notlp[n])
+		print ("")
+		
+#nambah kontak code
+def addcontact():	
+	name.append(input("Name : "))
+	notlp.append(input("Phone No. : "))
+	print("")
+	
+#menucode
+print(' Welcome !')
+while True:
+	print("####Menu####")
+	print("1. Contact List")
+	print("2. Add Contact")
+	print("3. Exit")
+	menu = int(input("Choose Menu : "))
+	
+	if menu == 1:
+		contactlist()
+	elif menu == 2:
+		addcontact()
+	elif menu == 3:
+		print(" Programe Done, See You !")
+		break
+	else :
+		print ("Menu Not Available\n")
